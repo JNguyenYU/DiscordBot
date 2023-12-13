2c6915a22e5941498ac9109c705c53e65f3d5b7f
EC2 Default User <ec2-user@ip-172-31-26-44.ec2.internal>
2023-12-13 03:50:03 +0000

    Finalized working discord bot

--- my_bot.py
+++ my_bot.py
@@ -2,13 +2,14 @@
 import discord
 import os
 import random
+from ec2_metadata import ec2_metadata
 from dotenv import load_dotenv
 
 #Initialize the variables
-load_dotenv()
 
-client = discord.Bot()
-token = os.getenv('TOKEN')
+load_dotenv()
+client = discord.Client()
+token = str(os.getenv('TOKEN'))
 
 #Intializing the Bot
 @client.event
@@ -28,19 +29,24 @@ async def on_message(message):
         return
     
     if channel == "random":
-        if user_message.lower() == "hello" or user_message.lower() == "hi":
-            await message.channel.send(f'Hello {username}')
+        if user_message.lower() == "hello world" or user_message.lower() == "hi":
+            await message.channel.send(f'Hello!')
             return
         elif user_message.lower() == "bye":
             await message.channel.send(f'Bye {username}')
         elif user_message.lower() == "tell me a joke":
             jokes = [" Can someone please shed more\
-                     light on how my lamp got stolen?",
+light on how my lamp got stolen?",
                      "Why is she called llene? She\
-                        stands on equal legs.",
+stands on equal legs.",
                         "What do you call a gazelle in a \
-                            lions territory? Denzel."]
+lions territory? Denzel."]
             await message.channel.send(random.choice(jokes))
+        elif user_message == "Tell me about my server!":
+            await message.channel.send(f"Your EC2 Data:\nRegion: {ec2_metadata.region}\nIP Address: {ec2_metadata.public_ipv4}\nAvailability: {ec2_metadata.availability_zone}\nServer instance:  {ec2_metadata.instance_type}")
+
+
 
 #Function client takes the token as an argument
 client.run(token)         
+
